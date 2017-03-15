0920b369b6e47c4ff46c8beade82cc282f0fe563
MSarandev <maxsarandev@gmail.com>
2017-03-15 22:54:24 +0000

    Major Update 3

--- interDes/MAX/Final_Proper/Main/script.js
+++ interDes/MAX/Final_Proper/Main/script.js
@@ -1,61 +1,66 @@
 // functions to change the text of the buttons
 
-var selector_music = 0;  // used to control what callback to attach
-var selector_movies = 0; // used to control what callback to attach
-var selector_books = 0;  // used to control what callback to attach
+var selector = 0;  // used to control what callback to attach
 
 function loadMe(){
-    selector_music = 0;  // reset the var
-    selector_movies = 0; // reset the var
-    selector_books = 0; // reset the var
+    selector = 0;  // reset the var
 }
 
 function displMusic(){
-    // fetch the elements
-    var element_top = document.getElementById("menu_img_top");
-    var element_lft = document.getElementById("menu_img_left");
-    var element_rgt = document.getElementById("menu_img_right");
+    if(selector == 0) {
+        // fetch the elements
+        var element_top = document.getElementById("menu_img_top");
+        var element_lft = document.getElementById("menu_img_left");
+        var element_rgt = document.getElementById("menu_img_right");
 
-    // change the img
-    element_top.src = "menu/MUSIC/house.png";
-    element_lft.src = "menu/MUSIC/pop.png";
-    element_rgt.src = "menu/MUSIC/rock.png";
+        // change the img
+        element_top.src = "menu/MUSIC/house.png";
+        element_lft.src = "menu/MUSIC/pop.png";
+        element_rgt.src = "menu/MUSIC/rock.png";
 
-    // update the control
-    selector_music = 1;
-
-    // change the btm button
-    changeBtn();
+        // update the control
+        selector = 1;
+    }else{
+        fetchMusic();
+    }
 }
 
 function displMovie(){
-    // fetch the elements
-    var element_top = document.getElementById("menu_img_top");
-    var element_lft = document.getElementById("menu_img_left");
-    var element_rgt = document.getElementById("menu_img_right");
+    if(selector == 0){
+        // fetch the elements
+        var element_top = document.getElementById("menu_img_top");
+        var element_lft = document.getElementById("menu_img_left");
+        var element_rgt = document.getElementById("menu_img_right");
 
-    // change the img
-    element_top.src = "menu/MOVIES/comedy.png";
-    element_lft.src = "menu/MOVIES/horror.png";
-    element_rgt.src = "menu/MOVIES/sci-fi.png";
+        // change the img
+        element_top.src = "menu/MOVIES/comedy.png";
+        element_lft.src = "menu/MOVIES/horror.png";
+        element_rgt.src = "menu/MOVIES/sci-fi.png";
 
-    // update the control
-    selector_movies = 1;
+        // update the control
+        selector = 1;
+    }else{
+        fetchMovie();
+    }
 }
 
 function displBooks(){
-    // fetch the elements
-    var element_top = document.getElementById("menu_img_top");
-    var element_lft = document.getElementById("menu_img_left");
-    var element_rgt = document.getElementById("menu_img_right");
+    if(selector == 0){
+        // fetch the elements
+        var element_top = document.getElementById("menu_img_top");
+        var element_lft = document.getElementById("menu_img_left");
+        var element_rgt = document.getElementById("menu_img_right");
 
-    // change the img
-    element_top.src = "menu/BOOKS/drama.png";
-    element_lft.src = "menu/BOOKS/fantasy.png";
-    element_rgt.src = "menu/BOOKS/poetry.png";
+        // change the img
+        element_top.src = "menu/BOOKS/drama.png";
+        element_lft.src = "menu/BOOKS/fantasy.png";
+        element_rgt.src = "menu/BOOKS/poetry.png";
 
-    // update the control
-    selector_books = 1;
+        // update the control
+        selector = 1;
+    }else{
+        fetchBooks();
+    }
 }
 
 // ~~~ ~~~ ~~~ ~~~ ~~~
@@ -63,13 +68,37 @@ function displBooks(){
 // functions to display the content
 
 function fetchMusic(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
 
+    // change the img
+    element_top.src = "menu/main/empty.png";
+    element_lft.src = "menu/main/empty.png";
+    element_rgt.src = "menu/main/empty.png";
 }
 
 function fetchMovie(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
 
+    // change the img
+    element_top.src = "menu/main/empty.png";
+    element_lft.src = "menu/main/empty.png";
+    element_rgt.src = "menu/main/empty.png";
 }
 
 function fetchBooks(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
 
+    // change the img
+    element_top.src = "menu/main/empty.png";
+    element_lft.src = "menu/main/empty.png";
+    element_rgt.src = "menu/main/empty.png";
 }
