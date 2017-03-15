3989c258379b11160f279842f8976f3b1ccd7d98
MSarandev <maxsarandev@gmail.com>
2017-03-15 20:57:08 +0000

    Major Update 1

---
+++ interDes/MAX/Final_Proper/Main/script.js
@@ -0,0 +1,53 @@
+// functions to change the text of the buttons
+
+function displMusic(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
+
+    // change the img
+    element_top.src = "menu/MUSIC/house.png";
+    element_lft.src = "menu/MUSIC/pop.png";
+    element_rgt.src = "menu/MUSIC/rock.png";
+}
+
+function displMovie(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
+
+    // change the img
+    element_top.src = "menu/MUSIC/house.png";
+    element_lft.src = "menu/MUSIC/pop.png";
+    element_rgt.src = "menu/MUSIC/rock.png";
+}
+
+function displSongs(){
+    // fetch the elements
+    var element_top = document.getElementById("menu_img_top");
+    var element_lft = document.getElementById("menu_img_left");
+    var element_rgt = document.getElementById("menu_img_right");
+
+    // change the img
+    element_top.src = "menu/MUSIC/house.png";
+    element_lft.src = "menu/MUSIC/pop.png";
+    element_rgt.src = "menu/MUSIC/rock.png";
+}
+
+// ~~~ ~~~ ~~~ ~~~ ~~~
+
+// functions to display the content
+
+function fetchMusic(){
+
+}
+
+function fetchMovie(){
+
+}
+
+function fetchSongs(){
+
+}
