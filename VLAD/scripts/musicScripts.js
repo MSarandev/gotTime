549485f5c59c6b4d9796dcd58b22d86c6d3df93b
Vladimir R <bladimip@gmail.com>
2017-04-12 08:19:44 +0100

    deezer api and konami code
    deezer api working - generates random song of selected genre;
    some genres not working (not present in deezer service) - either change
    them or do some workarounds;
    konami code working;

--- VLAD/scripts/musicScripts.js
+++ VLAD/scripts/musicScripts.js
@@ -2,31 +2,31 @@
 //Vladimir R.
 //9/4/2017
 
-//deezerNo - number of a radio(playlist) genre, used for generating iframe
+//deezerID - number of a radio(playlist) genre, used for generating iframe
 //i have assigned deezer radios only to genres that were present in MIX category of http://developers.deezer.com/musicplugins/player
 //array containing all music genres
 var musicGenres = [
-	{name:"Blues", deezerNo:"30921"},
-	{name:"Caribbean", deezerNo:""},
-	{name:"Classical", deezerNo:"30661"},
-	{name:"Country", deezerNo:"36801"},
-	{name:"Dance", deezerNo:"30951"},
-	{name:"Disco", deezerNo:"30841"},
-	{name:"Easy-listening", deezerNo:""},
-	{name:"Electronic", deezerNo:"30621"},
-	{name:"Folk", deezerNo:"37091"},
-	{name:"Hip-hop", deezerNo:"30991"},
-	{name:"House", deezerNo:""},
-	{name:"Jazz", deezerNo:"31031"},
-	{name:"Latin", deezerNo:"36791"},
-	{name:"Metal", deezerNo:"30901"},
-	{name:"Pop", deezerNo:"31061"},
-	{name:"Reggae", deezerNo:"31091"},
-	{name:"RnB", deezerNo:"30881"},
-	{name:"Rock", deezerNo:"37765"},
-	{name:"Techno", deezerNo:"30851"},
-	{name:"Trance", deezerNo:""},
-	{name:"World", deezerNo:""},
+	{name:"Blues", deezerID:"153"},
+	{name:"Caribbean", deezerID:""},
+	{name:"Classical", deezerID:"98"},
+	{name:"Country", deezerID:"84"},
+	{name:"Dance", deezerID:"113"},
+	{name:"Disco", deezerID:""},
+	{name:"Easy-listening", deezerID:""},
+	{name:"Electronic", deezerID:"106"},
+	{name:"Folk", deezerID:"466"},
+	{name:"Hip-hop", deezerID:"116"},
+	{name:"House", deezerID:""},
+	{name:"Jazz", deezerID:"129"},
+	{name:"Latin", deezerID:"197"},
+	{name:"Metal", deezerID:"464"},
+	{name:"Pop", deezerID:"132"},
+	{name:"Reggae", deezerID:"144"},
+	{name:"RnB", deezerID:"165"},
+	{name:"Rock", deezerID:"152"},
+	{name:"Techno", deezerID:""},
+	{name:"Trance", deezerID:""},
+	{name:"World", deezerID:""},
 ];
 
 //array containing few randomly chosen genres
@@ -62,23 +62,29 @@ function setButtons(){
 	//document.getElementById("test1").innerHTML += "Selected genres: " + selectedMusicGenres.toString();
 }
 
-function outputDeezer(g){
-	document.getElementById('result').innerHTML = '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=radio&id=radio-' + g.deezerNo + '&app_id=1" width="700" height="95"></iframe>';
-}
-
 //function that runs upon button click - WIP
 function outputGenre(g){
-	if (g.deezerNo != "") {
-		outputDeezer(g);
+	if (g.deezerID != "") {
+		document.getElementById("main").innerHTML += "<br>" + g.name + ", Deezer ID: " + g.deezerID;
+		getDeezerArtists(g);
 	} else {
-		document.getElementById('main').innerHTML+='<br>' + g.name + ' has no deezer radio assigned';
+		document.getElementById('main').innerHTML+='<br>' + g.name + ' has no Deezer ID assigned';
 	}
 }
 
-function playDatSong2(){
+//for querying deezer, ive decided to use three similar functions (they seem easy to optimize into one function with extra parameters but that might be somewhat tricky)
+//first function gets list of artists of given genre and chooses one
+//second function gets list of albums of given artist and chooses one
+//third function gets list of tracks of given album and chooses one, that later is put into html as iframe
+//ive left some output as comments in case of need in future
+
+//need to use JSONP or will get No 'Access-Control-Allow-Origin' error
+function getDeezerArtists(g){
 	// Using YQL and JSONP
 	$.ajax({
-		url: "http://api.deezer.com/artist/119",
+		//build url for the request
+		//need to add '&output=jsonp&callback=JSONP_CALLBACK' bit at the end or will get Unexpected token error
+		url: "http://api.deezer.com/genre/" + g.deezerID + "/artists&output=jsonp&callback=JSONP_CALLBACK",
 		// the name of the callback parameter, as specified by the YQL service
 		jsonp: "callback",
 		// tell jQuery we're expecting JSONP
@@ -87,41 +93,98 @@ function playDatSong2(){
 		data: {format: "json"},
 
 		// work with the response
-		success: function( response ) {
-				//create a string to contain our HTML code to inject
-				var htmlstring = "";
-				//iterate over the collection of results
-				for (var i=0; i<10; i++){
-					var title = response.Search[i].name;
-					htmlstring += "<li>" + title + "</li>";
-				}
+		success: function(response) {
+			// //create a string to contain our HTML code to inject - FOR TESTING
+			// var htmlstring = "";
+			// for (var i=0; i<response.data.length; i++){
+				// htmlstring += "<br>" + response.data[i].name + ", ID: " + response.data[i].id;
+			// }
+			// //display all artists of selected genre
+			// document.getElementById('result').innerHTML ='<br>' + htmlstring;
+			
+			//selecting one artist
+			var generated = Math.floor(Math.random() * response.data.length);
+			document.getElementById('main').innerHTML += '<br>Selected artist: ' + response.data[generated].name + ", ID: " + response.data[generated].id;
+			getDeezerAlbums(response.data[generated].id);
+		} //parameter success end
+	}); //ajax end
+} //function getDeezerArtists end
+
+function getDeezerAlbums(artistID) {
+	$.ajax({
+		url: "http://api.deezer.com/artist/" + artistID + "/albums&output=jsonp&callback=JSONP_CALLBACK",
+		jsonp: "callback",
+		dataType: "jsonp",
+		data: {format: "json"},
+		success: function(response) {
+			// var htmlstring = "";
+			// for (var i=0; i<response.data.length; i++){
+				// htmlstring += "<br>" + response.data[i].title + ", ID: " + response.data[i].id;
+			// }
+			// document.getElementById('result').innerHTML +='<br>' + htmlstring;
+			var generated = Math.floor(Math.random() * response.data.length);
+			document.getElementById('main').innerHTML += '<br>Selected album: ' + response.data[generated].title + ", ID: " + response.data[generated].id;
+			getDeezerSongs(response.data[generated].id);
+		} //parameter success end
+	}); //ajax end
+}
+
+function getDeezerSongs(albumID) {
+	$.ajax({
+		url: "http://api.deezer.com/album/" + albumID + "/tracks&output=jsonp&callback=JSONP_CALLBACK",
+		jsonp: "callback",
+		dataType: "jsonp",
+		data: {format: "json"},
+		success: function(response) {
+			// var htmlstring = "";
+			// for (var i=0; i<response.data.length; i++){
+				// htmlstring += "<br>" + response.data[i].title + ", ID: " + response.data[i].id;
+			// }
+			// document.getElementById('result').innerHTML +='<br>' + htmlstring;
+			var generated = Math.floor(Math.random() * response.data.length);
+			document.getElementById('main').innerHTML += '<br>Selected track: ' + response.data[generated].title + ", ID: " + response.data[generated].id;
+			outputDeezerSong(response.data[generated].id);
+		} //parameter success end
+	}); //ajax end
+}
+
+//displaying iframe with selected song - iframe paramters can be tweaked
+function outputDeezerSong(songID){
+	document.getElementById('result').innerHTML = '<iframe scrolling="no" frameborder="0" allowTransparency="true" src="http://www.deezer.com/plugins/player?format=classic&autoplay=false&playlist=true&width=700&height=350&color=007FEB&layout=dark&size=medium&type=tracks&id=' + songID + '&app_id=1" width="700" height="350"></iframe>';
+}
+
+//function for executing konami code on page
+//Konami code - up, up, down, down, left, rigth, left, right, B, A
+//code below is "ilja" (not case sensitive)
+function konamiCode() {
+	//array variable for storing input
+	var enteredCode = [];
+	//actions performed after key press
+	$('html').keydown(function (event) {
+		//adding new key to array
+		enteredCode.push(event.keyCode);
+		//limiting size of array to last X keys (saves memory) - make sure array is at least same length as longest code
+		if (enteredCode.length > 10) {
+				enteredCode.shift();
 		}
-	
-	//inject the HTML into our empty list
-	//$("#result").html("kaka");
-	});
-}
 
-function playDatSong(){
-	//call deezer API using AJAX
-
-		//use jquery json shortcut
-		$.getJSON("http://api.deezer.com/artist/119", function(jsondata){
-			//handle the results
-			addResultTitles(jsondata);
-		});
+		//FOR TESTING - CAN BE REMOVED
+		//console.log("Entered key code: " + event.keyCode.toString());
+		//console.log("Contents of array: " + enteredCode.toString());
 		
-}
-
-function addResultTitles(jsondata) {
-	//create a string to contain our HTML code to inject
-	var htmlstring = "";
-	//iterate over the collection of results
-	for (var i=0; i<10; i++){
-		var title = jsondata.Search[i].name;
-		htmlstring += "<li>" + title + "</li>";
-	}
-	
-	//inject the HTML into our empty list
-	$("#result").html(htmlstring);
+		//checking if input matches konami code
+		if (enteredCode.slice(-10).toString() == "38,38,40,40,37,39,37,39,66,65") {
+			//KONAMI CODE SUCCESS!
+			console.log("KONAMI CODE! - Now try typing team member's name");
+			enteredCode = [];
+		}
+		//checking if input matches 2nd code
+		if (enteredCode.slice(-4).toString() == "73,76,74,65") {
+			console.log('Everything is designed by Ilja Magdenko (with little help of George Clooney)');
+			$("body").html("<img src='http://www.newyorker.com/wp-content/uploads/2008/04/080414_r17095_p646-320.jpg' height='300'>" +
+			"<br><h1>Ilja Magdenko</h1><br><p>Desing By Ilja. and bla bla bla..</p>");
+			$('img').animate({'height':'1000', 'align':'middle'}, 50000);
+			enteredCode = [];
+		}
+	});//key press actions end
 }
