a623804e02c62d8580c89a76bc21357f1ee415fb
Ilja <duntiknews@yandex.ru>
2017-04-18 20:23:17 +0100

    Movie v1
    Now it's show movies without category instructions.

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -270,7 +270,50 @@ function clickMeVideo(){
 //FILM API GOES HERE
 function fetchFilmByGenre(filmGenre){
   //code that runs upon film genre button press
-  document.getElementById("generatedResult").innerHTML = '<p>Film - ' + filmGenre + '</p>';
+  //document.getElementById("generatedResult").innerHTML = '<p>Film!!! - ' + filmGenre + '</p>';
+  document.getElementById("generatedResult").innerHTML = '';
+  var randomzz = Math.floor((Math.random() * 100) + 1);
+
+  
+
+  $.ajax({
+    url: "https://api.themoviedb.org/3/movie/popular?page="+ randomzz +"&language=en-US&api_key=9c0081be84efec2670ae12b04e837e62",
+    dataType: "json",
+
+    success: function(data) {
+      for(i = 0; i < 2; i++){
+
+        var randomindex = Math.floor((Math.random() * 20) + 1);
+        do {
+          randomindex = Math.floor((Math.random() * 20) + 1);
+        } while(randomindex === ri);
+        generatedResult.innerHTML += "<center><img src=https://image.tmdb.org/t/p/w500" + data.results[randomindex].poster_path +" width=" + "189"+" height="+"255"+" alt="+"lorem"+"></center>"
+          generatedResult.innerHTML += "<div class="+"feature-content"+">"
+          generatedResult.innerHTML += "<h4><center>" + data.results[randomindex].title + "</center></h4>"
+          generatedResult.innerHTML += "<div class="+"table-responsive"+">"
+          generatedResult.innerHTML += "<table class="+"table"+">"
+          generatedResult.innerHTML += "<tr>"
+          generatedResult.innerHTML += "<td><b>Release Date: </b></td>"
+          generatedResult.innerHTML += data.results[randomindex].release_date
+          generatedResult.innerHTML += "</br>"
+          generatedResult.innerHTML += "<td><b>Grade: </b></td>"
+          generatedResult.innerHTML += data.results[randomindex].vote_average
+          generatedResult.innerHTML += "</br>"
+          generatedResult.innerHTML += "<td><b>Description: </b></td>"
+          generatedResult.innerHTML += data.results[randomindex].overview
+          generatedResult.innerHTML += "</br>"
+          generatedResult.innerHTML += "</tr>"
+          generatedResult.innerHTML += "</table>"
+          generatedResult.innerHTML += "</div>"
+          generatedResult.innerHTML += "</br>"
+          generatedResult.innerHTML += "</br>"
+        var ri = randomindex;
+
+      }
+    },
+
+    type: 'GET'
+  });
 }
 
 
@@ -302,45 +345,45 @@ function fetchBookByGenre(bookGenre){
     var randomz = Math.floor((Math.random() * 100) + 1);
 
     $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes?q=subject:" + search + "&startIndex=" + randomz + "&language:en",
-        dataType: "json",
+      url: "https://www.googleapis.com/books/v1/volumes?q=subject:" + search + "&startIndex=" + randomz + "&language:en",
+      dataType: "json",
 
-        success: function(data) {
-            for(i = 0; i < 2; i++){
-                result.innerHTML += "<center><img src=" + data.items[i].volumeInfo.imageLinks.thumbnail +" width=" + "189"+" height="+"255"+" alt="+"lorem"+"></center>"
-                result.innerHTML += "<div class="+"feature-content"+">"
-                result.innerHTML += "<h4><center>" + data.items[i].volumeInfo.title + "</center></h4>"
-                result.innerHTML += "<div class="+"table-responsive"+">"
-                result.innerHTML += "<table class="+"table"+">"
-                result.innerHTML += "<tr>"
-                result.innerHTML += "<td><b>Publication Date: </b></td>"
-                result.innerHTML += data.items[i].volumeInfo.publishedDate
-                result.innerHTML += "</br>"
-                result.innerHTML += "<td><b>Categories: </b></td>"
-                result.innerHTML += data.items[i].volumeInfo.categories
-                result.innerHTML += "</br>"
-                result.innerHTML += "<td><b>Pages: </b></td>"
-                result.innerHTML += data.items[i].volumeInfo.pageCount
-                result.innerHTML += "</br>"
-                result.innerHTML += "<td><b>Description: </b></td>"
-                result.innerHTML += data.items[i].volumeInfo.description
-                result.innerHTML += "</br>"
-                result.innerHTML += "</tr>"
-                result.innerHTML += "</table>"
-                result.innerHTML += "</div>"
-                result.innerHTML += "</br>"
-                result.innerHTML += "<a href=" + data.items[i].saleInfo.buyLink + " target=" + "_blank" + "><center><button type=" + "button" + "class=" + "btn btn-success" + ">Buy the Book</button></center></a>"
-                result.innerHTML += "</br>"
-                result.innerHTML += "</div>"
-                result.innerHTML += "</br>"
-                result.innerHTML += "</br>"
+      success: function(data) {
+        for(i = 0; i < 2; i++){
+          result.innerHTML += "<center><img src=" + data.items[i].volumeInfo.imageLinks.thumbnail +" width=" + "189"+" height="+"255"+" alt="+"lorem"+"></center>"
+          result.innerHTML += "<div class="+"feature-content"+">"
+          result.innerHTML += "<h4><center>" + data.items[i].volumeInfo.title + "</center></h4>"
+          result.innerHTML += "<div class="+"table-responsive"+">"
+          result.innerHTML += "<table class="+"table"+">"
+          result.innerHTML += "<tr>"
+          result.innerHTML += "<td><b>Publication Date: </b></td>"
+          result.innerHTML += data.items[i].volumeInfo.publishedDate
+          result.innerHTML += "</br>"
+          result.innerHTML += "<td><b>Categories: </b></td>"
+          result.innerHTML += data.items[i].volumeInfo.categories
+          result.innerHTML += "</br>"
+          result.innerHTML += "<td><b>Pages: </b></td>"
+          result.innerHTML += data.items[i].volumeInfo.pageCount
+          result.innerHTML += "</br>"
+          result.innerHTML += "<td><b>Description: </b></td>"
+          result.innerHTML += data.items[i].volumeInfo.description
+          result.innerHTML += "</br>"
+          result.innerHTML += "</tr>"
+          result.innerHTML += "</table>"
+          result.innerHTML += "</div>"
+          result.innerHTML += "</br>"
+          result.innerHTML += "<a href=" + data.items[i].saleInfo.buyLink + " target=" + "_blank" + "><center><button type=" + "button" + "class=" + "btn btn-success" + ">Buy the Book</button></center></a>"
+          result.innerHTML += "</br>"
+          result.innerHTML += "</div>"
+          result.innerHTML += "</br>"
+          result.innerHTML += "</br>"
 
-            }
-        },
+        }
+      },
 
-        type: 'GET'
+      type: 'GET'
     });
-}
+  }
 
 
 
@@ -353,8 +396,8 @@ function fetchVideoByGenre(videoGenre){
         gapi.client.setApiKey('AIzaSyCSceFCV5K0_4O6Rn52sm-ywlvH1UXLBGw'); // assign the API key
         gapi.client.load('youtube', 'v3', function () { // Youtube authentication
             makeRequest(); // More youtube authentication
-        });
-    });
+          });
+      });
 
     function makeRequest() {
         // define the request
@@ -363,7 +406,7 @@ function fetchVideoByGenre(videoGenre){
             q: q, // keyword
             part: 'snippet', // specify what to return
             maxResults: 2 // results limit
-        });
+          });
 
         // execute the request
         request.execute(function(response)  {
@@ -378,15 +421,15 @@ function fetchVideoByGenre(videoGenre){
 
                 // create new container for each video
                 $('#generatedResult').append('<span>'+
-                    vidTitle+
-                    '</br>' +
+                  vidTitle+
+                  '</br>' +
                     // define the iframe and plug src
                     '<iframe src="'+urlString+'" width="800px" height="600px"></iframe>' +
                     '</span>' + '</br></br>');
-            })
-        })
+              })
+          })
+      }
     }
-}
 
 
 
@@ -455,7 +498,7 @@ function konamiCode() {
     if (enteredCode.slice(-4).toString() == "73,76,74,65") {
       console.log('Everything is designed by Ilja Magdenko (with little help of George Clooney)');
       $("body").html('<img src="http://www.newyorker.com/wp-content/uploads/2008/04/080414_r17095_p646-320.jpg" height="300">' +
-      "<br><h1>Ilja Magdenko</h1><br><p>Desing By Ilja. and bla bla bla..</p>");
+        "<br><h1>Ilja Magdenko</h1><br><p>Desing By Ilja. and bla bla bla..</p>");
       $("img").animate({'height':'1000', 'align':'middle'}, 50000);
       enteredCode = [];
     }
