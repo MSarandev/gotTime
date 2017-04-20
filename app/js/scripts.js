d10df757e92b4198e0b918ca1964a9c233663586
MSarandev <maxsarandev@gmail.com>
2017-04-20 19:55:10 +0100

    Konami update

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -467,13 +467,15 @@ function fetchVideoByGenre(videoGenre){
           });
       });
 
+    var storedURLs = []; //init
+
     function makeRequest() {
         // define the request
         var q = videoGenre; // give the query a keyword (FROM GENRE LIST)
         var request = gapi.client.youtube.search.list({ // youtube request code
             q: q, // keyword
             part: 'snippet', // specify what to return
-            maxResults: 2 // results limit
+            maxResults: 50 // results limit
           });
 
         // execute the request
@@ -487,14 +489,28 @@ function fetchVideoByGenre(videoGenre){
                 // fetch the URL and prepare for embedding
                 var urlString = "https://www.youtube.com/embed/" + item.id.videoId;
 
+                // push to the array
+                storedURLs.push(urlString);
+
+                console.log("URLs loaded. Count: " + storedURLs.length);
+              });
+
+
+            var rand_num = Math.floor((Math.random() * 50) + 1); // create rand var
+
+            // for X amount (def. 3) pull vids
+            for(var i = 0; i<3; i++){
                 // create new container for each video
                 $('#generatedResult').append('<span>'+
-                  vidTitle+
-                  '</br>' +
+                    vidTitle+
+                    '</br>' +
                     // define the iframe and plug src
-                    '<iframe src="'+urlString+'" width="800px" height="600px"></iframe>' +
+                    '<iframe src="'+storedURLs[rand_num]+'" width="inherit%" height="600px"></iframe>' +
                     '</span>' + '</br></br>');
-              })
+
+                // get new rand. var
+                rand_num = Math.floor((Math.random() * 50) + 1); // create rand var
+            }
           })
       }
     }
@@ -559,6 +575,68 @@ function konamiCode() {
     //checking if input matches konami code
     if (enteredCode.slice(-10).toString() == "38,38,40,40,37,39,37,39,66,65") {
       //KONAMI CODE SUCCESS!
+
+        // LAST EDIT: Max 20/04
+
+
+        // All code is taken from: http://thecodeplayer.com/walkthrough/matrix-rain-animation-html5-canvas-javascript
+
+        // push to the body
+        $("body").html('<canvas id="c"></canvas>'+
+        '<style>*{margin: 0; padding: 0;}canvas {display: block;}</style>');
+
+        // MAKE IT RAIN
+        var c = document.getElementById("c");
+        var ctx = c.getContext("2d");
+
+        //making the canvas full screen
+        c.height = window.innerHeight;
+        c.width = window.innerWidth;
+
+        //chinese characters - taken from the unicode charset
+        var chinese = "田由甲申甴电甶男甸甹町画甼甽甾甿畀畁畂畃畄畅畆畇畈畉畊畋界畍畎畏畐畑";
+        //converting the string into an array of single characters
+        chinese = chinese.split("");
+
+        var font_size = 10;
+        var columns = c.width/font_size; //number of columns for the rain
+        //an array of drops - one per column
+        var drops = [];
+        //x below is the x coordinate
+        //1 = y co-ordinate of the drop(same for every drop initially)
+        for(var x = 0; x < columns; x++)
+            drops[x] = 1;
+
+        //drawing the characters
+        function draw()
+        {
+            //Black BG for the canvas
+            //translucent BG to show trail
+            ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+            ctx.fillRect(0, 0, c.width, c.height);
+
+            ctx.fillStyle = "#0F0"; //green text
+            ctx.font = font_size + "px arial";
+            //looping over drops
+            for(var i = 0; i < drops.length; i++)
+            {
+                //a random chinese character to print
+                var text = chinese[Math.floor(Math.random()*chinese.length)];
+                //x = i*font_size, y = value of drops[i]*font_size
+                ctx.fillText(text, i*font_size, drops[i]*font_size);
+
+                //sending the drop back to the top randomly after it has crossed the screen
+                //adding a randomness to the reset to make the drops scattered on the Y axis
+                if(drops[i]*font_size > c.height && Math.random() > 0.975)
+                    drops[i] = 0;
+
+                //incrementing Y coordinate
+                drops[i]++;
+            }
+        }
+
+        setInterval(draw, 33);
+
       console.log("KONAMI CODE! - Now try typing team member's name");
       enteredCode = [];
     }
